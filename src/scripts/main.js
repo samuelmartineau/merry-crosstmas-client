efe7820b0b1fa8e9b5d807be46713a463a693cf0
Samuel Martineau <samuel.martineau@axa.fr>
2015-10-03 13:45:25 +0200

    release

--- src/scripts/main.js
+++
@@ -1,4 +0,0 @@
-window.utils = require('./utils.js');
-window.ContactModel = require('./models/contact.js');
-window.smoothScroll = require('./../../node_modules/smoothscroll/smoothscroll.js');
-window.Quill = require('./../../node_modules/quill/dist/quill.js');
